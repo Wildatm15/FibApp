1744ed5dc7e16b5d3bd8d80462821ed535a73bd0
Wildatm15 <109327035+Wildatm15@users.noreply.github.com>
2022-07-15 16:39:31 +0700

    Add files via upload

---
+++ app.R
@@ -0,0 +1,36 @@
+library(shiny)
+
+ui = fluidPage(
+  titlePanel("Fibonacci"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      helpText("Print the Fibonacci Number and it's inverse"),
+      sliderInput("n", label = "Number n for Fibonacci:", min = 0, max = 40, value =5)
+    ),
+    mainPanel(
+      textOutput("fib_num"),
+      textOutput("fib_num_inv")
+    )
+  )
+)
+
+fib = function(n){
+  ifelse(n<3, 1, fib(n-1)+fib(n-2))
+}
+
+server = function(input, output){
+  currentFib = function(){
+    fib(as.numeric(input$n))
+  }
+  
+  output$fib_num = renderText({
+    paste("Fibonacci number for the selected number ", input$n, "is = ", currentFib())
+  })
+  
+  output$fib_num_inv = renderText({
+    paste("Inverse of the fibonacci number of  ", input$n, "is = ", 1/currentFib())
+  })
+}
+
+shinyApp(ui = ui, server = server)
